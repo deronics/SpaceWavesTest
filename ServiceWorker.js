f1695cd28be0ffb97d004711c161661203e0bf9a
deronics <77541322+deronics@users.noreply.github.com>
2024-11-01 17:50:41 +1000

    Add files via upload

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -1,9 +1,9 @@
 const cacheName = "do.Games-Space Waves-1.0-preview";
 const contentToCache = [
     "Build/dae5c7bd2b15915f51ae4e56b8f5367d.loader.js",
-    "Build/5dfa30058b1d90b744ff6c6af17eda02.framework.js.unityweb",
-    "Build/074a7f82949e2b9f66743c4b858dfda0.data.unityweb",
-    "Build/df7efc714c53f75210aaf79f09fb12f2.wasm.unityweb",
+    "Build/aaa5936fb06815d56cbd6de4e5bee7a1.framework.js.unityweb",
+    "Build/ff72c2ee999cee7245e7af63c5f31eb9.data.unityweb",
+    "Build/6b7309af8312e2df7e88ff6a0b08d88b.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
